09237aae67ad553342ecea63949d95151076f19a
it_dog <it_dog_zhang@163.com>
2020-09-27 20:40:21 +0800

    2020/927

--- live-node/helloWord/helloworld.js
+++
@@ -1,9 +0,0 @@
-
-const http = require('http')
-
-const  server =  http.createServer()
-
-server.on("request",(request)=>{
-    console.log(request)
-})
-server.listen(8080)
--- live-react/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-//store/index.js
-import { createStore } from 'redux'
-import reducer from './reducers/index'
-const store = createStore(reducer)
-export default store
