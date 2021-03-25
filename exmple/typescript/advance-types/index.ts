24e50aa44c81c513d42328ae560f2073ba53356a
it_dog_zhang <it_dog_zhang@163.com>
2021-03-25 21:13:14 +0800

    2021/3/25

--- exmple/typescript/advance-types/index.ts
+++ exmple/typescript/advance-types/index.ts
@@ -1,37 +1,39 @@
-function longestCommonPrefix(strs: string[]): string {
-  let str = '';
-  if(strs.length === 0){
-    return ""
-  }
-  if(strs.length === 1){
-    return strs[0]
-  }
-  let num = strs[0].length-1;
-  for (const item of strs) {
-    if(item.length-1 <num){
-      num = item.length-1
-    }
-  }
-  let i =0
-  while(num>=i){
-    let key = strs[0].charAt(i)
-    let check = true
-    for (const item of strs) {
-      if(item.charAt(i) !== key){
-        check = false
-        break;
-      }
-    }
-    if(check === false){
-      break;
-    }else{
-      str +=key
-    }
-    i++
-  }
-  return str
+function isValid(s: string): boolean {
+   const arr = s.split('')
+   if(arr.length%2!==0){
+     return false
+   }else{
+     const point:{[key:string]:string} = {
+      '(': ')',
+      '[': ']',
+      '{': '}',
+      ')' : '(',
+      ']' : '[',
+      '}' : '{'
+     }
+     const statck:any = []
+     for (const item of arr) {
+       if(statck.length == 0){
+         statck.push(item)
+         continue
+       }else{
+        
+         const topr =statck.pop()
+         const top = point[topr] 
+         if(top != item){
+           statck.push(topr)
+           statck.push(item)
+         }else{
+           continue
+         }
+       }
+     }
+     if(statck.length === 0){
+       return true
+     }else{
+       return false
+     }
+   }
+};
 
-}
-console.log(longestCommonPrefix(["flower","flower","flower","flower"]
-))
-
+console.log( isValid('{[]}'))
